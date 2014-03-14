4578a1d8f8c06d52f47fad2f1910ce8f894b2011
VLanvin <victor.lanvin@ens-cachan.fr>
2014-03-14 09:07:17 +0100

    Majuscules

--- src/main/scala/crypto_system.scala
+++
@@ -1,4 +0,0 @@
-trait CryptoSystem {
-  def encrypt(msg:String, pub:PublicKey, rdm:Int): String
-  def decrypt(msg:String, priv: PrivateKey): String
-}
--- src/main/scala/group.scala
+++
@@ -1,7 +0,0 @@
-trait Group[E] {
-  val unit : E
-  val generator : E
-  def times (e1: E, e2: E): E
-  def exp (e: E, n: Int): E = {
-  }
-}
