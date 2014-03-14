df955dd28cbb5abdd67149aeaddf37b3359536ca
Saroupille <franth2@gmail.com>
2014-03-14 09:14:43 +0100

    implement fast expo

--- src/main/scala/Group.scala
+++ src/main/scala/Group.scala
@@ -3,5 +3,13 @@ trait Group[E] {
   val generator : E
   def times (e1: E, e2: E): E
   def exp (e: E, n: Int): E = {
+    if(n==0)
+      return unit
+    else if(n==1)
+      return e
+    else if(n%2==0)
+      return exp(times(e,e), n/2)
+    else
+      return times(e,exp(times(e,e), (n-1)/2))
   }
 }
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,46 +1,5 @@
-import scala.collection.mutable.Map
+
 
 object Test {
 
-  def interpretationTest() : Unit = { 
-    println("###################")
-    println("Unit tests on terms")
-    println("###################")
-    val x= TermVariable("x")
-    println("Result expected : x")
-    println("Result          : "+x.interprete(Map()))
-    val y= TermVariable("y")
-    val pair1= TermPair(x,y)
-    println("Result expected : pair(x,y)")
-    println("Result          : "+pair1.interprete(Map()))
-    val proj1=TermProj1(pair1)
-    println("Result expected : x")
-    println("Result          : "+proj1.interprete(Map()))
-    val proj2=TermProj2(pair1)
-    println("Result expected : y")
-    println("Result          : "+proj2.interprete(Map()))
-    val list1=TermList(List(x,y,pair1,proj1,proj2))
-    println("Result expected : x::y::pair(x,y)::x::y::[]")
-    println("Result          : "+list1.interprete(Map()))
-  }
-
-  def parseTest() : Unit = {
-    def printTest (l : List[Proc]) : Unit = {
-        l match {
-            case Nil => println ("");
-            case t::q => println(t.toString()); printTest(q)
-        }
-    }
-
-    def interpreteTest (l: List[Proc]) : Unit = {
-        l match {
-            case Nil => println ("End of interpretation");
-            case t::q => t.interprete(Map()); printTest(q)
-        }
-    }
-
-    val parser = new Parser(new AsynchroneousStrategy());
-    val l = parser.parseFile("testNSL.txt");
-    interpreteTest(l);
-  }
 }
